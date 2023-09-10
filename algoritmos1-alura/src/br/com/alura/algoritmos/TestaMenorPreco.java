ae3ab865362fc1a150ccde1883f4e708fc5e4d0d
Vinicius Cruz <cruzvindev@gmail.com>
2023-09-10 16:56:04 -0300

    Refatorando o código para fincionar em qualquer linguagem

--- algoritmos1-alura/src/br/com/alura/algoritmos/TestaMenorPreco.java
+++ algoritmos1-alura/src/br/com/alura/algoritmos/TestaMenorPreco.java
@@ -11,16 +11,20 @@ public class TestaMenorPreco {
                 new Produto("Fusca", 17000)
         };
 
-        int maisBarato = 0;
-
-        for(int atual = 0; atual <= 4; atual++){
-            if(produtos[atual].getPreco() < produtos[maisBarato].getPreco()){
-                maisBarato = atual;
-            }
-        }
+        int maisBarato = buscaMenor(produtos, 0, 4);
 
         System.out.println(maisBarato);
         System.out.println("O carro " + produtos[maisBarato].getNome() + " é o mais barato, e custa " +
                   produtos[maisBarato].getPreco());
+    }
+
+    private static int buscaMenor(Produto[] produtos,int inicio,  int termino) {
+        int maisBarato = 0;
+        for(int atual = inicio; atual <= termino; atual++){
+            if(produtos[atual].getPreco() < produtos[maisBarato].getPreco()){
+                maisBarato = atual;
+            }
+        }
+        return maisBarato;
     }
 }
